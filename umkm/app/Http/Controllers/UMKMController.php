14a8e17ecf60421d97a9acd0a39d593ad9998572
Andreyas3667 <andreyasming@gmail.com>
2025-03-16 22:41:18 +0700

    Update
    Routing

--- umkm/app/Http/Controllers/UMKMController.php
+++ umkm/app/Http/Controllers/UMKMController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UMKM;
 use Illuminate\Http\Request;
 
 class UMKMController extends Controller
 {
-    //
+    public function index()
+{
+    $umkm = UMKM::select('nama_usaha', 'alamat', 'latitude', 'longitude')->get();
+    return view('maps', compact('umkm'));
+}
+
+
+
 }
--- umkm/routes/web.php
+++ umkm/routes/web.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\UMKMController;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\TransaksiController;
 
+
 Route::get('/umkm', [UMKMController::class, 'index']);
 Route::get('/umkm/{id}', [UMKMController::class, 'show']);
 Route::post('/umkm', [UMKMController::class, 'store']);
+Route::get('/maps', [UMKMController::class, 'index']);
 
 
 Route::get('/', function () {
